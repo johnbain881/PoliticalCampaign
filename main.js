31de1c70f0be1deeec1c611b6067a3da72ba228c
John Bain <johnknoxbain@gmail.com>
2020-04-21 13:18:55 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const campaign = {
+    statements = {
+        taxes: "",
+        jobs: "",
+        infrastructure: "",
+        healthCare: "",
+        crimeAndEnforcement: ""
+    },
+    donationForm: "url",
+    calenderDates: [],
+    volunteerInformation: [],
+    biography: "",
+    imageGallery: {
+        headshot: "url",
+        family: "",
+        constituents: ""
+    },
+    missionStatement: "",
+    voteUrl: ""
+}
